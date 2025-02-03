a82464735f9f4d91597522aad3ba3996f19bc80b
christine <nessymungla5@gmail.com>
2025-02-03 15:38:21 +0300

    update cart

---
+++ Admin/Cart/update_cart.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if (isset($_POST['product_id']) && isset($_POST['quantity'])) {
+    $product_id = $_POST['product_id'];
+    $quantity = intval($_POST['quantity']);
+
+    if (isset($_SESSION['cart'][$product_id])) {
+        $_SESSION['cart'][$product_id]['quantity'] = $quantity;
+    }
+}
+
+header("Location: cart.php");
+exit;
+?>
